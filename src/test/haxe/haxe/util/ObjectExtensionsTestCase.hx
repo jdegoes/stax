9310413f5a708cb91f619031846dbe9a37ee8d99
John A. De Goes <john@socialmedia.com>
2010-08-28 08:44:29 -0700

    fix error in object extensions

--- src/test/haxe/haxe/util/ObjectExtensionsTestCase.hx
+++ src/test/haxe/haxe/util/ObjectExtensionsTestCase.hx
@@ -39,4 +39,14 @@ class ObjectExtensionsTestCase extends TestCase {
 	  
 	  assertEquals("baz", o.setAny("foo", "baz").getAny("foo").get());
 	}
+	
+	public function testReplaceAll() {
+	  var o = { foo: "bar", bar: "foo" };
+	  
+	  var replaced = o.replaceAllAny({foo: "foo"}, '');
+	  
+	  assertEquals("bar", replaced.getAny("foo").get());
+	  
+	  assertTrue(replaced.getAny("bar").isEmpty());
+	}
 }
