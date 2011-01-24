7408e1e4bb2e5eec31a1d1a56d6da2c8590e22cd
Paul DeGoes <pauldegoes@hotmail.com>
2011-01-24 16:03:16 -0700

    added HTMLEventExtensions.hx

---
+++ src/main/haxe/js/dom/HTMLEventExtensions.hx
@@ -0,0 +1,112 @@
+/*
+ HaXe library written by Paul M. De Goes <paul@socialmedia.com> and John A. De Goes <john@socialmedia.com>
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the
+ distribution.
+
+ THIS SOFTWARE IS PROVIDED BY SOCIAL MEDIA NETWORKS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SOCIAL MEDIA NETWORKS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package js.dom;
+
+import Dom;
+import js.Env;
+import Prelude;
+import PreludeExtensions;
+
+using PreludeExtensions;
+
+/** 
+ * Comm]on elements.
+ */
+class HTMLDocumentExtensions {
+  public static function newElement(document: HTMLDocument, eType: String, ?atts: Array<Tuple2<String, String>> = null,  ?style: String = ""): HTMLElement {
+    var element = document.createElement(eType);
+    
+    for (i in 0...atts.length) {
+      element.setAttribute(atts[i]._1, atts[i]._2);
+    }
+    
+    element.style.cssText = style;
+    return element;
+  }
+  
+  public static function newImage(doc: HTMLDocument): HTMLImageElement {
+    return cast doc.createElement('IMG');
+  }
+  
+  public static function newDiv(doc: HTMLDocument): HTMLDivElement {
+    return cast doc.createElement('DIV');
+  }
+  
+  public static function newIframe(doc: HTMLDocument, ?width: Int, ?height: Int): HTMLIFrameElement {
+    var iframe: HTMLIFrameElement = cast doc.createElement('IFRAME');
+    
+    width.toOption().zip(height.toOption()).map(function(t) {
+      iframe.setAttribute('width',   width.toString());
+      iframe.setAttribute('height',  height.toString());
+    });
+    
+    return iframe;
+  }
+  
+  /** Creates a new iframe intended to be used as a window. This iframe will 
+   * not have borders, will not allow resizing, will not have any margin or
+   * padding, and will be transparent (depending on the body).
+   */
+  public static function newIframeWindow(doc: HTMLDocument, width: Int, height: Int): HTMLIFrameElement {
+    var iframe = newIframe(doc, width, height);
+    
+    iframe.setAttribute('frameBorder',   '0');
+    iframe.setAttribute('marginWidth',   '0');
+    iframe.setAttribute('marginHeight',  '0');
+    iframe.setAttribute('vspace',        '0');
+    iframe.setAttribute('hspace',        '0');
+    iframe.setAttribute('scrolling',     'no');
+    iframe.setAttribute('noResize',      'noResize');
+    iframe.setAttribute('allowTransparency', 'true');
+    
+    iframe.style.margin            = '0';
+    iframe.style.padding           = '0';
+    iframe.style.border            = 'none';
+    iframe.style.borderLeftStyle   = 'none';
+    iframe.style.borderRightStyle  = 'none';
+    iframe.style.borderTopStyle    = 'none';
+    iframe.style.borderBottomStyle = 'none';
+    iframe.style.backgroundColor   = 'transparent';
+    
+    return iframe;
+  }
+  
+  public static function newIframeInvisible(doc: HTMLDocument): HTMLIFrameElement {
+    return newIframeWindow(doc, 0, 0);
+  }
+  
+  public static function getId(doc: HTMLDocument, s: String): HTMLElement {
+    return doc.getElementById(s);
+  }
+  
+  public static function getIds(doc: HTMLDocument, a: Array<String>): Array<HTMLElement> {
+    var result: Array<HTMLElement> = [];
+    
+    for (i in 0...a.length) {
+      result.push(getId(doc, a[i]));
+    }
+    
+    return result;
+  }
+  
+  public static function getTags(doc: HTMLDocument, s: String): Array<HTMLElement> {
+    return OptionExtensions.getOrElseC(OptionExtensions.toOption( cast doc.getElementsByTagName(s)), []);
+  }
+  
+  public static function getClasses(doc: HTMLDocument, s: String): Array<HTMLElement> {
+    return OptionExtensions.getOrElseC(OptionExtensions.toOption(Env.getElementsByClass(s)), []);
+  }
+}
---
+++ src/test/haxe/js/dom/HTMLDocumentExtensionsTestCase.hx
@@ -0,0 +1,104 @@
+/*
+ HaXe library written by Paul M. De Goes <paul@socialmedia.com>
+ Contributed by Social Media Networks
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the
+ distribution.
+
+ THIS SOFTWARE IS PROVIDED BY SOCIAL MEDIA NETWORKS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SOCIAL MEDIA NETWORKS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package js.dom;
+
+import Dom;
+import js.Env;
+import js.dom.Quirks;
+import js.dom.HTMLElementExtensions;
+import Prelude;
+import PreludeExtensions;
+using  js.dom.HTMLElementExtensions;
+using  js.dom.HTMLDocumentExtensions;
+
+
+import haxe.test.TestCase;
+
+class HTMLDocumentExtensionsTestCase extends TestCase {
+    var _doc:           Env.HTMLDocument;
+    var _testElements:  HTMLElement;
+
+    public function new():Void {
+        super();
+        _doc = Env.document;
+        _testElements = _doc.newElement("div", [Tuple2.create("id", "testElements")]);
+        _doc.getElementsByTagName("body")[0].appendChild(_testElements);
+    }
+    
+    public function testThatNewElementWorks(): Void {
+      var e = _doc.newElement("img", [ Tuple2.create("id", "foo"), Tuple2.create("src", "http://thenextweb.com/apps/files/2010/03/google_logo.jpg")], "width: 300px; height: 100px; position: relative;");
+      
+      asTestElement(e);
+      
+      assertTrue(e != null);
+      assertEquals(e.getAttribute("src"), "http://thenextweb.com/apps/files/2010/03/google_logo.jpg");
+      assertEquals(e.getAttribute("id"), "foo"); 
+      
+      clearTestElements();
+    }
+    
+    public function testThatGetIdWorks(): Void {
+      var e = asTestElement(_doc.newElement("div", [Tuple2.create("id", "getIdElem")]));
+      
+      assertTrue(_doc.getId("getIdElem") != null);
+    }
+    
+    public function testThatGetTagsWorks(): Void {
+      var e = _doc.getTags("body")[0];
+      
+      assertEquals(e.nodeName.toLowerCase(), "body");
+    }
+    
+    public function testThatGetClassesWorks(): Void {
+      var e1 = asTestElement(_doc.newElement("div", [Tuple2.create("class", "test-elem bob")]));
+      var e2 = asTestElement(_doc.newElement("div", [Tuple2.create("class", "test-elem foo fang")]));
+      var e3 = asTestElement(_doc.newElement("div", [Tuple2.create("class", "foo test-elem")]));
+      
+      var body = _doc.getTags("body")[0];
+      
+      e1.appendChild(e2);
+      e2.appendChild(e3);
+      
+      asTestElement(e1);
+      
+      
+      assertEquals(_doc.getClasses("bob").length, 1);
+      assertEquals(_doc.getClasses("test-elem").length, 3);
+      assertEquals(_doc.getClasses("foo").length, 2);
+      assertEquals(_doc.getClasses("blat").length, 0);
+      
+      clearTestElements();
+    }
+    
+    
+    
+    
+    
+    
+    
+    private function asTestElement(e: HTMLElement): HTMLElement {
+      _testElements.appendChild(e);
+      return e;
+    }
+    
+    private function clearTestElements() {
+      
+      _testElements.innerHTML = "";
+    }
+
+}
