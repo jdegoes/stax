35a44d760060d81166875ebe6e00637b87facc89
Paul DeGoes <pauldegoes@hotmail.com>
2011-02-08 15:04:50 -0700

    Added DomCollecionExtensions class

---
+++ src/main/haxe/js/dom/DomCollectionExtensions.hx
@@ -0,0 +1,34 @@
+/*
+ HaXe library written by Paul M. De Goes <paul@socialmedia.com> and John A. De Goes <john@socialmedia.com>
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the
+ distribution.
+
+ THIS SOFTWARE IS PROVIDED BY SOCIAL MEDIA NETWORKS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SOCIAL MEDIA NETWORKS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package js.dom;
+
+import Prelude;
+import Dom;
+
+using haxe.data.collections.ArrayExtensions;
+using PreludeExtensions;
+using js.dom.HTMLDocumentExtensions;
+
+class DomCollectionExtensions {
+
+  asArray(c: DomCollection<HTMLElement>): Array<HTMLElement> {
+    var result: Array<HTMLElement> = cast c;
+    return result;
+  }
+  
+}
+  
+  
