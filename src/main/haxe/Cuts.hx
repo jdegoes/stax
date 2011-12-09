f4b22fc0a831fe0ed6f6a690f1229126a59a679f
0b1Kn00b <polysemantic@gmail.com>
2011-12-09 12:31:16 +0100

    Short cuts.

---
+++ src/main/haxe/Cuts.hx
@@ -0,0 +1,14 @@
+package ;
+
+/**
+ * ...
+ * @author 0b1kn00b
+ */
+
+class Cuts {
+
+	
+}
+
+typedef Arrays 		= haxe.data.collections.ArrayExtensions;
+typedef Iterables = haxe.data.collections.IterableExtensions;
